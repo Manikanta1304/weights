68ed3b6a80d7a7ef2796f72fb2cac79900819ffc
Manikanta <69647433+Manikanta1304@users.noreply.github.com>
2023-03-10 17:05:55 +0530

    Update streamlit-grid-v3.py

--- streamlit-grid-v3.py
+++ streamlit-grid-v3.py
@@ -3,15 +3,28 @@ import os
 from PIL import Image
 
 # Set page title and favicon
-st.set_page_config(page_title='Image Explorer', page_icon=':camera:')
+st.set_page_config(page_title='Image Explorer', page_icon=':camera:', layout='wide')
 
 # Set up sidebar
 st.sidebar.title('Image Explorer')
-directory = st.sidebar.text_input('Enter directory path:', './images-dir')
+directory = st.sidebar.selectbox('Select a directory:', ['./images-dir'] + os.listdir(), index=0)
 
 # Get image paths
 image_extensions = ['.jpg', '.jpeg', '.png']
 image_paths = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(tuple(image_extensions))]
+
+# Set up sidebar for selecting an image to display in the sidebar
+st.sidebar.write('### Select an image:')
+selected_image_path = st.sidebar.selectbox('Select an image:', image_paths)
+
+# Create a container for the selected image
+selected_image_container = st.sidebar.container()
+
+# Display the selected image in the sidebar
+with selected_image_container:
+    selected_image = Image.open(selected_image_path)
+    selected_image.thumbnail((600, 600))
+    st.image(selected_image, use_column_width=True, caption=os.path.basename(selected_image_path))
 
 # Define number of columns in the grid
 cols = st.sidebar.slider('Number of columns:', 2, 6, 4)
@@ -21,9 +34,6 @@ image_size = 200
 
 # Create a container for the image grid
 container = st.container()
-
-# Create a container for the selected image
-selected_image_container = st.container()
 
 # Calculate number of rows in the grid
 rows = len(image_paths) // cols + 1
@@ -46,10 +56,11 @@ with container:
             i = index // cols
             grid[i][j].image(image, use_column_width=True, caption=os.path.basename(image_path))
             if grid[i][j].button('Select', key=f'select_button_{index}'):
-                selected_image = Image.open(image_path)
+                selected_image_path = image_path
+                selected_image = Image.open(selected_image_path)
                 selected_image.thumbnail((600, 600))
                 selected_image_container.empty()
-                selected_image_container.image(selected_image, use_column_width=True, caption=os.path.basename(image_path))
+                selected_image_container.image(selected_image, use_column_width=True, caption=os.path.basename(selected_image_path))
 
 # Set the container height to display a scrollbar when there are many images
 container_height = str(rows * 210) + 'px'  # 210px is the height of each row
